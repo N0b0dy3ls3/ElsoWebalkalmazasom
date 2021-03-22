daacfe4ac22c8a784b85ee2cf3a92bfe050e5431
N0b0dy3ls3 <80622760+N0b0dy3ls3@users.noreply.github.com>
2021-03-22 22:43:56 +0100

    Update scripts.js
    Kep tomb befejezve

---
+++ scripts.js
@@ -0,0 +1,16 @@
+let data = [{ photo: 'WeboldalKépek/1.jpg', title: 'Mohás szobrok', description: 'Mohás szobrok'},
+{ photo: 'WeboldalKépek/2.jpg', title: 'Templom a sziklában', description: 'Templom a sziklában'},
+{ photo: 'WeboldalKépek/3.jpg', title: 'Dalai Láma', description: 'Dalai Láma'},
+{ photo: 'WeboldalKépek/4.jpg', title: 'Olvasó szerzetes', description: 'Olvasó szerzetes'},
+{ photo: 'WeboldalKépek/5.jpg', title: 'Meditáló lányok', description: 'Meditáló lányok'},
+{ photo: 'WeboldalKépek/6.jpg', title: 'Templomkert', description: 'Templomkert'},
+{ photo: 'WeboldalKépek/7.jpg', title: 'Virágszírmok', description: 'Virágszírmok'},
+{ photo: 'WeboldalKépek/8.jpg', title: 'Olvasó szerzetes II', description: 'Olvasó szerzetes II'},
+{ photo: 'WeboldalKépek/9.jpg', title: 'Imamalmok', description: 'Imamalmok'},
+{ photo: 'WeboldalKépek/10.jpg', title: 'Fény ünnepe, talán', description: 'Fény ünnepe, talán'},
+{ photo: 'WeboldalKépek/11.jpg', title: 'Gyerek szerzetesek', description: 'Gyerek szerzetesek'},
+{ photo: 'WeboldalKépek/12.jpg', title: 'Ifjú szerzetes', description: 'Ifjú szerzetes'}];
+
+
+
+        
